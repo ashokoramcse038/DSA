242e6e1d7426d460f6fc6c206fe3a6fdc2e494ac
ashokoramcse038 <ashokoramcse038@gmail.com>
2026-08-05 12:16:35 +0530

    add this DSA part

--- TCS_NQT/prime_number_range.cpp
+++ TCS_NQT/prime_number_range.cpp
@@ -1,1 +1,32 @@
 #include<iostream>
+using namespace std;
+bool isprime(int n){
+      if(n<=1){
+            return false;
+      }
+            for(int i=2; i<n; i++){
+                  if(n%i == 0){
+                        return false;
+                  }
+                  
+            }
+            return true;
+      }
+
+void foundprime(int l,int r){
+      bool found = false;
+      for(int i=l; i<=r; i++){
+            if(isprime(i)){
+                  cout<<" "<<i;
+                  found = true;
+            }
+      }
+      if(!found){
+            cout<<"Not prime number.";
+      }
+}
+int main(){
+      int l=10,r=30;
+      
+      foundprime(l,r);
+}
